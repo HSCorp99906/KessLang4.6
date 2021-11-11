2b4c23fbdd92fc9973bf5e26dc262c6266c73640
Ian <teaqllabs@gmail.com>
2021-11-10 19:50:59 -0500

    Added function call demo.

--- src/Parser.c
+++ src/Parser.c
@@ -38,7 +38,7 @@ void parse(parser_t* parser) {
                 }
 
                 error = 1;
-                printf("\033[91m\nERROR: Missing semicolon. \n\n%s\n%s", parser -> currentToken.line, pointBuf);
+                printf("\033[91m\nERROR: Missing semicolon. \n\n%s\n%s\n\n", parser -> currentToken.line, pointBuf);
                 break;
             }
         }
@@ -46,17 +46,42 @@ void parse(parser_t* parser) {
         if (parser -> currentToken.type == T_LPAREN) {
             ++lparenc;
         } else if (parser -> currentToken.type == T_RPAREN) {
+
+            if (lparenc < 1) {
+                printf("\033[91m\nERROR: Syntax error. \n\n%s\n\n", parser -> currentToken.line);
+                break;
+            }
+
             ++rparenc;
         }
 
         if (parser -> currentToken.character == ';') {
             if (lparenc < rparenc || lparenc > rparenc) {
-                printf("\033[91m\nERROR: Unmatched parenthesis. \n\n%s\n", parser -> currentToken.line);
+                printf("\033[91m\nERROR: Unmatched parenthesis. \n\n%s\n\n", parser -> currentToken.line);
                 break;
             }
 
             lparenc = 0;
             rparenc = 0;
+        }
+
+        if (lparenc > 0 && rparenc > 0) {
+            /* This will have to be changed later
+            *  Due to the abiliy to create functions.
+            */
+
+            char nameBuf[500];
+            memset(nameBuf, 0, 500);
+
+            for (int i = 0; i < strlen(parser -> currentToken.line) && i < 500; ++i) {
+                if (parser -> currentToken.line[i] == '(') {
+                    break;
+                }
+
+                strncat(nameBuf, &parser -> currentToken.line[i], 1);
+            }
+
+            printf("FUNCTION CALL: %s\n", nameBuf);
         }
 
         advance(parser);
