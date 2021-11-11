7920b83bc92cd1c9d400b3c7abc745d1b79d92b1
Ian <teaqllabs@gmail.com>
2021-11-11 01:01:47 -0500

    Added executer.

---
+++ include/Executer.h
@@ -0,0 +1,9 @@
+#ifndef KESS_EXECUTER_H_
+#define KESS_EXECUTER_H_
+
+#include "AST.h"
+
+
+void execute(ast_t* ast);
+
+#endif
---
+++ src/Executer.c
@@ -0,0 +1,8 @@
+#include "../include/Executer.h"
+
+
+void execute(ast_t* ast) {
+    for (int i = 0; i < ast -> treesize; ++i) {
+
+    }
+}
